0a31aae1ad0fe199852ed3e4d29c9579a0f0d854
Czap Balázs <balazsczap@gmail.com>
2016-12-05 01:03:35 +0100

    day3 part2

--- 3/adv3.cpp
+++ 3/adv3.cpp
@@ -2,13 +2,12 @@
 #include <fstream>
 #include <cstdio>
 #include <iomanip>
-
+#include <vector>
+#include <string>
 
 bool isPossible(int a, int b, int c){
 	return a+b>c && a+c>b && b+c>a;
 }
-
-
 
 int main(){
 	std::ifstream stream;
@@ -16,14 +15,26 @@ int main(){
 	int count = 0;
 	int possibleCount = 0;
 	for (
-		std::string line; std::getline(stream, line); 
-		) {
-        int a;
-        int b;
-        int c;
-        sscanf(line.c_str(), "%d %d %d", &a,&b,&c);  
-       	count++;
-       	isPossible(a,b,c) ?  possibleCount++ : possibleCount;
+		std::vector<std::string> lines{3}; 
+		std::getline(stream, lines[0]) && std::getline(stream, lines[1]) && std::getline(stream, lines[2]);
+		) 
+	{
+		std::vector<std::vector<int>> sides{};
+
+		for(int i=0; i<3;++i){
+			std::vector<int> side{0,0,0};
+			int a,b,c;
+	        sscanf(lines[i].c_str(), "%d %d %d", &a,&b,&c);  
+	        side[0] = a;
+			side[1] = b;
+			side[2] = c;
+	       	count++;
+	       	sides.push_back(side);
+		}
+		for(int i=0; i<3;++i){
+			isPossible(sides[0][i], sides[1][i], sides[2][i]) ?  possibleCount++ : possibleCount;
+		}
+
     }
 	stream.close();
 	std::cout<<"All: " << count << " Possible: " << possibleCount;
