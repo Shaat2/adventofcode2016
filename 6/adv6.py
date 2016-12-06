2d6ef4232abb05d4316bd04b76453c31f8b02579
Czap Balázs <balazsczap@gmail.com>
2016-12-06 23:35:07 +0100

    day6 part1

---
+++ 6/adv6.py
@@ -0,0 +1,24 @@
+import string
+from operator import itemgetter
+from collections import OrderedDict
+lines = []
+with open("test.txt") as f:
+	lines = f.readlines()
+
+index_counts = [{} for i in range(len(lines[0]))]
+print index_counts
+
+for line in lines:
+	for idx, letter in enumerate(line):
+		if letter not in index_counts[idx]:
+			index_counts[idx][letter]=1;
+		else:
+			index_counts[idx][letter]+=1;
+
+
+decoded = ""
+
+for di in index_counts:
+	decoded = decoded + sorted(di.items(),key=lambda t: t[1], reverse=True)[0][0]
+
+print decoded
